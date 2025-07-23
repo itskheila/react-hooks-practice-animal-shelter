e609dbd422c97d11b3fe97c2a324293f2a733201
itskheila <munenetiara@gmail.com>
2025-07-23 11:21:57 +0300

    assignment submission

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,28 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  const onChangeType = (type) => {
+    setFilters({ type });
+  };
+
+  const onFindPetsClick = () => {
+    let endpoint = "http://localhost:3001/pets";
+    if (filters.type !== "all") {
+      endpoint += `?type=${filters.type}`;
+    }
+
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(data => setPets(data));
+  };
+
+  const onAdoptPet = (id) => {
+    const updatedPets = pets.map(pet => 
+      pet.id === id ? { ...pet, isAdopted: true } : pet
+    );
+    setPets(updatedPets);
+  };
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +37,14 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters 
+              onChangeType={onChangeType} 
+              onFindPetsClick={onFindPetsClick}
+              filters={filters}
+            />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
